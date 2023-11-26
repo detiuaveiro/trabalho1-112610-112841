d8ca3d549f0930128df7f0c538c80e680c7db7ab
David Amorim <davidfilipeamorim@gmail.com>
2023-11-26 12:30:59 +0000

    Add tests for ImageBlur

--- imageTest.c
+++ imageTest.c
@@ -28,6 +28,12 @@ const char* IMAGES[] = {
     "./test/subimg_sm.pgm",
 };
 
+const char* IMAGES2[] = {
+    "./pgm/large/ireland_03_1600x1200.pgm",
+    "./pgm/medium/airfield-05_640x480.pgm",
+    "./pgm/small/bird_256x256.pgm",
+};
+
 void test_locate_subimage() {
   printf("# LOAD image\n");
   InstrReset();  // to reset instrumentation
@@ -105,12 +111,95 @@ void test_locate_subimage() {
   ImageDestroy(&subimg_sm);
 }
 
+void test_blur() {
+  printf("# LOAD image\n");
+  InstrReset();  // to reset instrumentation
+  Image large = ImageLoad(IMAGES2[0]);
+  Image medium = ImageLoad(IMAGES2[1]);
+  Image small = ImageLoad(IMAGES2[2]);
+
+  if (large == NULL) {
+    error(2, errno, "Loading %s: %s", IMAGES2[0], ImageErrMsg());
+  }
+  if (medium == NULL) {
+    error(2, errno, "Loading %s: %s", IMAGES2[1], ImageErrMsg());
+  }
+  if (small == NULL) {
+    error(2, errno, "Loading %s: %s", IMAGES2[2], ImageErrMsg());
+  }
+  InstrPrint();  // to print instrumentation
+
+  int dx = 40, dy = 20;
+  printf("# ImageBlur (Most optimized) (1600x1200) with blur window (40x20)\n");
+  InstrReset();  // to reset instrumentation
+  ImageBlur(large, dx, dy);
+  InstrPrint();  // to print instrumentation
+  printf("# ImageBlur2 (Half optimized) (1600x1200) with blur window (40x20)\n");
+  InstrReset();  // to reset instrumentation
+  ImageBlur2(large, dx, dy);
+  InstrPrint();  // to print instrumentation
+  printf("# ImageBlur3 (Not optimized) (1600x1200) with blur window (40x20)\n");
+  InstrReset();  // to reset instrumentation
+  ImageBlur3(large, dx, dy);
+  InstrPrint();  // to print instrumentation
+
+  dx = 40, dy = 40;
+
+  printf("# ImageBlur (Most optimized) (640x480) with blur window (40x40)\n");
+  InstrReset();  // to reset instrumentation
+  ImageBlur(medium, dx, dy);
+  InstrPrint();  // to print instrumentation
+  printf("# ImageBlur2 (Half optimized) (640x480) with blur window (40x40)\n");
+  InstrReset();  // to reset instrumentation
+  ImageBlur2(medium, dx, dy);
+  InstrPrint();  // to print instrumentation
+  printf("# ImageBlur3 (Not optimized) (640x480) with blur window (40x40)\n");
+  InstrReset();  // to reset instrumentation
+  ImageBlur3(medium, dx, dy);
+  InstrPrint();  // to print instrumentation
+
+  dx = 20, dy = 20;
+
+  printf("# ImageBlur (Most optimized) (640x480) with blur window (20x20)\n");
+  InstrReset();  // to reset instrumentation
+  ImageBlur(medium, dx, dy);
+  InstrPrint();  // to print instrumentation
+  printf("# ImageBlur2 (Half optimized) (640x480) with blur window (20x20)\n");
+  InstrReset();  // to reset instrumentation
+  ImageBlur2(medium, dx, dy);
+  InstrPrint();  // to print instrumentation
+  printf("# ImageBlur3 (Not optimized) (640x480) with blur window (20x20)\n");
+  InstrReset();  // to reset instrumentation
+  ImageBlur3(medium, dx, dy);
+  InstrPrint();  // to print instrumentation
+
+  dx = 4, dy = 2;
+
+  printf("# ImageBlur (Most optimized) (256x256) with blur window (4x2)\n");
+  InstrReset();  // to reset instrumentation
+  ImageBlur(small, dx, dy);
+  InstrPrint();  // to print instrumentation
+  printf("# ImageBlur2 (Half optimized) (256x256) with blur window (4x2)\n");
+  InstrReset();  // to reset instrumentation
+  ImageBlur2(small, dx, dy);
+  InstrPrint();  // to print instrumentation
+  printf("# ImageBlur3 (Not optimized) (256x256) with blur window (4x2)\n");
+  InstrReset();  // to reset instrumentation
+  ImageBlur3(small, dx, dy);
+  InstrPrint();  // to print instrumentation
+
+  ImageDestroy(&large);
+  ImageDestroy(&medium);
+  ImageDestroy(&small);
+}
+
 int main(int argc, char* argv[]) {
   program_name = argv[0];
 
   ImageInit();
 
-  test_locate_subimage();
+  // test_locate_subimage();
+  test_blur();
 
   return 0;
 }
